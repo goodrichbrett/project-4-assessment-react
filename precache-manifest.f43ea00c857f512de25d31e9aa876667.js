b94a0486fb8a99ded1cc1947bcf94a370f8ca192
goodrichbrett <brettgoodrich@outlook.com>
2020-09-22 12:28:59 -0500

    Updates

---
+++ precache-manifest.f43ea00c857f512de25d31e9aa876667.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf8d0a8b9a712149d4a893dfe42f873",
+    "url": "/goodrichbrett/%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20project-4-assessment-react.git/index.html"
+  },
+  {
+    "revision": "2e5a4dd6be201700f4c9",
+    "url": "/goodrichbrett/%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20project-4-assessment-react.git/static/css/main.15dfb2c0.chunk.css"
+  },
+  {
+    "revision": "3ef72daff6719c125035",
+    "url": "/goodrichbrett/%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20project-4-assessment-react.git/static/js/2.7e260883.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goodrichbrett/%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20project-4-assessment-react.git/static/js/2.7e260883.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e5a4dd6be201700f4c9",
+    "url": "/goodrichbrett/%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20project-4-assessment-react.git/static/js/main.91c20d90.chunk.js"
+  },
+  {
+    "revision": "7fbefa6a4030b8361e96",
+    "url": "/goodrichbrett/%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20project-4-assessment-react.git/static/js/runtime-main.b1035abb.js"
+  }
+]);
